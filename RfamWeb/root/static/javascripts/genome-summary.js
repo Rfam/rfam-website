f4ccd147e58d9e77ddf573754b903254229c87fb
Anton Petrov <apetrov@ebi.ac.uk>
2019-01-21 09:17:31 +0000

    Use the main search index

--- RfamWeb/root/static/javascripts/genome-summary.js
+++ RfamWeb/root/static/javascripts/genome-summary.js
@@ -9,7 +9,7 @@ angular.module('rfamApp')
     controller: function($scope) {
 
       var search_config = {
-          ebeye_base_url: 'http://wwwdev.ebi.ac.uk/ebisearch/ws/rest/rfam',
+          ebeye_base_url: 'http://www.ebi.ac.uk/ebisearch/ws/rest/rfam',
           fields: [
               'id',
               'assembly_level',
