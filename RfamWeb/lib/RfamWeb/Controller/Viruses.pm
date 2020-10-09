658c0360368010472323b092dac98b0086827d93
Anton Petrov <apetrov@ebi.ac.uk>
2020-10-09 14:14:41 +0100

    Add a landing page for viruses

---
+++ RfamWeb/lib/RfamWeb/Controller/Viruses.pm
@@ -0,0 +1,24 @@
+
+package RfamWeb::Controller::Cloud;
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN {
+  extends 'Catalyst::Controller';
+}
+
+# __PACKAGE__->config( namespace => 'covid-19' );
+
+sub covid : Chained( '/' )
+          PathPart( 'viruses' )
+          Args( 0 ) {
+  my ( $this, $c ) = @_;
+
+  $c->log->debug('RfamWeb::Root::viruses: status message') if $c->debug;
+
+  $c->stash->{template} = 'pages/viruses.tt';
+}
+
+
+1;
