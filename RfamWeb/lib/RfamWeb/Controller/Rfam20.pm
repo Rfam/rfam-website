7f4ee287834091f6804f4cf30b323a7b99d39ca5
Emma Cooke <emma.cooke93@gmail.com>
2022-07-22 11:43:53 +0100

    test homepage link

--- RfamWeb/lib/RfamWeb/Controller/Rfam20.pm
+++ RfamWeb/lib/RfamWeb/Controller/Rfam20.pm
@@ -8,13 +8,13 @@ BEGIN {
 }
 
 sub covid : Chained( '/' )
-          PathPart( 'rfam_20' )
+          PathPart( 'rfam20' )
           Args( 0 ) {
   my ( $this, $c ) = @_;
 
   $c->log->debug('RfamWeb::Root::rfam20: status message') if $c->debug;
 
-  $c->stash->{template} = 'pages/rfam_20.tt';
+  $c->stash->{template} = 'pages/rfam20.tt';
 }
 
 
