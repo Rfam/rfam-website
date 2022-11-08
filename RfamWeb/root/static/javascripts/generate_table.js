0f0f2f97f46b720767fbde71901c71fcea781581
Blake Sweeney <bsweeney@ebi.ac.uk>
2022-11-08 11:43:10 +0000

    Clean up 3D table
    Adds a little text to the header, which I may add more to later if
    needed. Also improves the table display a little. It is cleaner to use
    ', ' as the separator as that way the table can resize and not be very
    wide. Additionally, links to the PDB structures were added and the names
    of links were changed.

--- RfamWeb/root/static/javascripts/generate_table.js
+++ RfamWeb/root/static/javascripts/generate_table.js
@@ -5,22 +5,21 @@ getData();
 async function getData() {
       const response = await fetch('static/data/3d-pdb.tsv')
       const data = await response.text();
-      console.log(data);
 
       const table = data.split('\n');
       table.forEach((row,index) => {
-      const columns = row.split('\t')
-      const id = columns[0]
-      const desc = columns[1]
-      const pdb = columns[2]
-      const seed = columns[3]
-      console.log(id, desc, pdb, seed);
+      const columns = row.split('\t');
+      const id = columns[0];
+      const desc = columns[1];
+      const pdb = columns[2].split(',').map(pdb => `<a href="https://www.ebi.ac.uk/pdbe/entry/pdb/${pdb}">${pdb}</a>`).join(', ')
+      const seed = columns[3];
+
       const tr = document.createElement("tr");
       tr.innerHTML = `
           <td><a href="/family/${id}">${id}</a></td>
           <td>${desc}</td>
           <td>${pdb}</td>
-          <td><a href="/family/${id}/alignment/stockholm?gzip=1&download=1">${id}.stockholm.txt.gz</a></td>
+          <td><a href="/family/${id}/alignment/stockholm?gzip=1&download=1">${id}.sto.gz</a></td>
           `;
        tableBody.append(tr);
 })
