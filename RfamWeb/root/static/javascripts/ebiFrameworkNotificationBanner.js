915ee98417396a8a0fbc7a09aeacc4a303607aa6
Blake Sweeney <bsweeney@ebi.ac.uk>
2025-04-14 13:19:52 +0100

    Update privacy banner notices
    Have to point at the PDFs for now. They will be updated at some point
    again this year.

--- RfamWeb/root/static/javascripts/ebiFrameworkNotificationBanner.js
+++ RfamWeb/root/static/javascripts/ebiFrameworkNotificationBanner.js
@@ -183,7 +183,7 @@ function ebiFrameworkRunDataProtectionBanner(targetedFrameworkVersion) {
     }
 
     // Default global values
-    dataProtectionSettings.message = 'This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our <a target="_blank" href="https://www.ebi.ac.uk/data-protection/privacy-notice/embl-ebi-public-website">Privacy Notice</a> and <a target="_blank" href="https://www.ebi.ac.uk/about/terms-of-use">Terms of Use</a>.';
+    dataProtectionSettings.message = 'This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our <a target="_blank" href="https://rfam.org/static/documents/privacy-notice.pdf">Privacy Notice</a> and <a target="_blank" href="https://www.ebi.ac.uk/about/terms-of-use">Terms of Use</a>.';
     dataProtectionSettings.serviceId = 'embl-ebi-public-website'; // use the URL stub from your DP record at http://content.ebi.ac.uk/list-data-protection-records
     dataProtectionSettings.dataProtectionVersion = '1.0';
 
