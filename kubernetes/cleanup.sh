871dcb90d9b74bf180a007d6a2976f15cb8ae6e2
pmustonebi <muston@ebi.ac.uk>
2025-08-08 11:14:04 +0100

    Coverted to helm deployment

--- kubernetes/cleanup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f apache-deployment.yaml --ignore-not-found=true
-kubectl delete -f rfam-deployment.yaml --ignore-not-found=true
-kubectl delete -f catalyst-service.yaml --ignore-not-found=true
-
--- kubernetes/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f services.yaml 
-kubectl apply -f rfam-deployment.yaml  
-kubectl apply -f apache-deployment.yaml
-kubectl apply -f ingress.yaml
