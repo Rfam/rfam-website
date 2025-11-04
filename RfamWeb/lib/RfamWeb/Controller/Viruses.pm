1c6f2607e357819eba73e188da2a582a4afc4f00
pmustonebi <muston@ebi.ac.uk>
2025-11-04 09:45:56 +0000

    Added redirect

--- RfamWeb/lib/RfamWeb/Controller/Viruses.pm
+++ RfamWeb/lib/RfamWeb/Controller/Viruses.pm
@@ -1,4 +1,3 @@
-
 package RfamWeb::Controller::Viruses;
 
 use Moose;
@@ -16,6 +15,17 @@ sub covid : Chained( '/' )
   $c->log->debug('RfamWeb::Root::viruses: status message') if $c->debug;
 
   $c->stash->{template} = 'pages/viruses.tt';
+}
+
+sub hcv : Chained( '/' )
+        PathPart( 'viruses/hcv' )
+        Args( 0 ) {
+  my ( $this, $c ) = @_;
+
+  $c->log->debug('RfamWeb::Viruses::hcv: redirecting to viruses page') if $c->debug;
+
+  # Redirect to the general viruses page with a 301 (permanent redirect)
+  $c->response->redirect( $c->uri_for('/viruses'), 301 );
 }
 
 
